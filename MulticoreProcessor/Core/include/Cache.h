2d85aca52e98cda6e3a55848213e0d17bcb158ed
ofir.g <ofir.g@solaredge.com>
2021-11-01 21:12:11 +0200

    cache start api add main Memory

--- MulticoreProcessor/Core/include/Cache.h
+++ MulticoreProcessor/Core/include/Cache.h
@@ -12,7 +12,6 @@
 \par
 ALL RIGHTS RESERVED
 *****************************************************************************/
-
 #ifndef __CACHE_H__
 #define __CACHE_H__
 
@@ -25,46 +24,80 @@ ALL RIGHTS RESERVED
 /************************************
 *      definitions                 *
 ************************************/
+#define CACHE_SIZE	256
+#define BLOCK_SIZE	4
+#define FRAME_SIZE	(CACHE_SIZE / BLOCK_SIZE)
 
 /************************************
 *       types                       *
 ************************************/
+typedef union
+{
+	uint16_t data;
 
+	struct
+	{
+		uint16_t tag : 12;	// [0:11]
+		uint16_t mesi : 2;	// [12:13]
+	}fields;
+} Tsram_s;
+
+typedef struct
+{
+	uint32_t dsram[CACHE_SIZE];
+	Tsram_s tsram[FRAME_SIZE];
+} CacheData_s;
 
 /************************************
 *       API                         *
 ************************************/
-
 /*!
 ******************************************************************************
 \brief
-Initialize func
+Initialize caching
 
 \details
 Must be called only once
 
 \param
- [in] counter_val - reset counter value
- [out] out_val    -
+ [in] cache_data - the cache data of specific core.
 
 \return none
 *****************************************************************************/
-void Cache_Init(void);
+void Cache_Init(CacheData_s* cache_data);
 
 /*!
 ******************************************************************************
 \brief
- Get data from cache
+ Read data from cache
 
 \details
  if address is not match the cache will take it from the main memory.
 
 \param
+ [in] cache_data - the cache data of specific core.
  [in] address - the memory address.
  [out] data   - the memory at the recieved address.
 
 \return true if address exist on cache.
 *****************************************************************************/
-bool Cache_GetData(uint32_t address, uint32_t* data);
+bool Cache_ReadData(CacheData_s* cache_data, uint32_t address, uint32_t* data);
+
+/*!
+******************************************************************************
+\brief
+ Write data from cache
+
+\details
+ if address is not match the cache will take it from the main memory.
+
+\param
+ [in] cache_data - the cache data of specific core.
+ [in] address - the memory address.
+ [in] data   - the data to write.
+
+\return true if address exist on cache.
+*****************************************************************************/
+bool Cache_WriteData(CacheData_s* cache_data, uint32_t address, uint32_t data);
 
 #endif // __CACHE_H__
