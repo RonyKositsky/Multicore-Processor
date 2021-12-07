fc5200339ad23a44f534ad0f2ff778ca896c0fd4
Yoantan33 <yoni.garti@gmail.com>
2021-12-07 18:48:16 +0200

    fix bug in counter, add nop after branch

--- Assembly tests/counter/counter0.asm
+++ Assembly tests/counter/counter0.asm
@@ -2,8 +2,6 @@
 #R3 - local counter
 #R4 - modulu result
 #R10 - my core number
-
-
 add $r10, $zero, $zero, 0		#pc:0, R10 = 0 my core number is 0 
 lw, $r2, $zero, $zero, 0		#pc:1, R2 = MEM[0]
 add, $r3, $zero, $imm, 128		#pc:2, R3 = 128, local counter
@@ -13,8 +11,10 @@ sub, $r3, $r3, $imm, 1			#pc:5, R3 = R3 - 1
 lw, $r2, $zero, $zero, 0		#pc:6, R2 = MEM[0]
 and $r4, $r2, $imm, 3			#pc:7, R4 = R2&3 (equivalent to MEM[0] modulu 4)
 bne, $imm, $r4, $r10, 6			#pc:8, if R4 != R10, jump to pc=6 (if not my turn, keep polling)
-add, $r2, $r2, $imm, 1			#pc:9, R2 = R2 + 1
-sw, $r2, $zero, $zero, 0		#pc:10, MEM[0] = R2
-sub, $r3, $r3, $imm, 1			#pc:11, R3 = R3 - 1
-bne, $imm, $r3, $zero, 6		#pc:12, if R3 != 0, jump to pc=6, else stop execution
-halt, $zero, $zero, $zero, 0	#pc:13, HALT
+add $zero, $zero, $zero, 0		#pc:9, nop
+add, $r2, $r2, $imm, 1			#pc:10, R2 = R2 + 1
+sw, $r2, $zero, $zero, 0		#pc:11, MEM[0] = R2
+sub, $r3, $r3, $imm, 1			#pc:12, R3 = R3 - 1
+bne, $imm, $r3, $zero, 6		#pc:13, if R3 != 0, jump to pc=6, else stop execution
+add $zero, $zero, $zero, 0		#pc:14, nop
+halt, $zero, $zero, $zero, 0	#pc:15, HALT
--- Assembly tests/mulserial/mulserial.asm
+++ Assembly tests/mulserial/mulserial.asm
@@ -13,45 +13,57 @@ add $r12, $zero, $zero, 0			#pc:10, R12 = 0, address inside first mat
 add $r13, $zero, $zero, 0			#pc:11, R13 = 0, calculation result, r
 add $r14, $zero, $zero, 0			#pc:12, R14 = 0, temp
 add $r15, $zero, $zero, 0			#pc:13, R15 = 0, address inside second mat 
-bge $imm, $r10, $r5, 55				#pc:14, if jj >= N, exit jj (main) loop
-add $r11, $zero, $zero, $zero		#pc:15, kk = 0
-bge $imm, $r11, $r5, 53				#pc:16, if kk >= N, exit kk loop
-add $r7, $zero, $zero, 0			#pc:17, i = 0
-bge $imm, $r7, $r5, 51				#pc:18, if i >= N, exit i loop
-add $r8, $zero, $r10, 0				#pc:19, j = jj
-add $r14, $r10, $imm, 8				#pc:20, temp = jj + B    j loop jumps here
-blt $imm, $r14, $r5, 23				#pc:21, if temp < N, skip next instruction
-add $r14, $zero, $r5, 0				#pc:22, temp = N
-bge $imm, $r14, $r8, 49				#pc:23, if min(jj + B, N) >= j, exit j loop
-add $r13, $zero, $zero, $zero		#pc:24, r = 0
-add $r9, $r10, $zero, 0				#pc:25, k = kk
-add $r14, $r11, $imm, 8				#pc:26, temp = kk + B    k loop jumps here
-blt $imm, $r14, $r5, 29				#pc:27, if temp < N, skip next instruction
-add $r14, $zero, $r5, 0				#pc:28, temp = N
-bge $imm, $r14, $r9, 41				#pc:29, if min(kk + B, N) >= k, exit k loop
-mul $r12, $r7, $r5, 0				#pc:30, address1 = i*N
-add $r12, $r12, $r9, 0				#pc:31, address1 = i*N + k = address1 in memory
-mul $r15, $r9, $r5, 0				#pc:32, address2 = k*N
-add $r15, $r15, $r8, 0				#pc:33, address2 = k*N + j
-add $r15, $r15, $r3, 0				#pc:34, address2 = address2 in memory
-lw $r2, $r12, $zero, 0				#pc:35, R2 = MEM[i*N + k] = Y[i][k]
-lw $r14, $r15, $zero, 0				#pc:36, R14 = MEM[k*N + j] = Z[k][j]
-mul $r6, $r2, $r14, 0				#pc:37, R6 = R2 * R14 = Y[i][k] * Z[k][j]
-add $r13, $r13, $r6, 0				#pc:38, r = r + Y[i][k] * Z[k][j]
-add $r9, $r9, $imm, 1				#pc:39, k++
-beq $imm, $zero, $zero, 26			#pc:40, jump to start of k loop
-mul $r2, $r7, $r5, 0				#pc:41, R2 = i * N
-add $r2, $r2, $r8, 0				#pc:42, R2 = i * N + j
-add $r2, $r2, $r4, 0				#pc:43, R2 = address of X[i][j] in memory
-lw $r6, $r2, $zero, 0				#pc:44, R6 = MEM[i*N + j] = X[i][j]
-add $r6, $r6, $r13, 0				#pc:45, R6 = R6 + r
-sw $r6, $r2, $zero, 0				#pc:46, X[i][j] = R6 = X[i][j] + r
-add $r8, $r8, $imm, 1				#pc:47, j++
-beq $imm, $zero, $zero, 20			#pc:48, jump to start of j loop
-add $r7, $r7, $imm, 1				#pc:49, i++
-beq $imm, $zero, $zero, 18			#pc:50, jump to start of i loop
-add $r11, $r11, $imm, 8				#pc:51, kk =  kk + B
-beq $imm, $zero, $zero, 16			#pc:52, jump to start of kk loop
-add $r10, $r10, $imm, 8				#pc:53, jj =  jj + B
-beq $imm, $zero, $zero, 14			#pc:54, jump to start of jj loop
-halt $zero, $zero, $zero, 0			#pc:55, HALT execution
+bge $imm, $r10, $r5, 67				#pc:14, if jj >= N, exit jj (main) loop
+add $zero, $zero, $zero, 0			#pc:15, nop
+add $r11, $zero, $zero, $zero		#pc:16, kk = 0
+bge $imm, $r11, $r5, 64				#pc:17, if kk >= N, exit kk loop	this was 16
+add $zero, $zero, $zero, 0			#pc:18, nop
+add $r7, $zero, $zero, 0			#pc:19, i = 0
+bge $imm, $r7, $r5, 61				#pc:20, if i >= N, exit i loop	this was 18
+add $zero, $zero, $zero, 0			#pc:21, nop
+add $r8, $zero, $r10, 0				#pc:22, j = jj
+add $r14, $r10, $imm, 8				#pc:23, temp = jj + B    j loop jumps here	this was 20
+blt $imm, $r14, $r5, 27				#pc:24, if temp < N, skip next instruction
+add $zero, $zero, $zero, 0			#pc:25, nop
+add $r14, $zero, $r5, 0				#pc:26, temp = N
+bge $imm, $r14, $r8, 58				#pc:27, if min(jj + B, N) >= j, exit j loop	this was 23
+add $zero, $zero, $zero, 0			#pc:28, nop
+add $r13, $zero, $zero, $zero		#pc:29, r = 0
+add $r9, $r10, $zero, 0				#pc:30, k = kk
+add $r14, $r11, $imm, 8				#pc:31, temp = kk + B    k loop jumps here	this was 26
+blt $imm, $r14, $r5, 35				#pc:32, if temp < N, skip next instruction
+add $zero, $zero, $zero, 0			#pc:33, nop
+add $r14, $zero, $r5, 0				#pc:34, temp = N
+bge $imm, $r14, $r9, 49				#pc:35, if min(kk + B, N) >= k, exit k loop	this was 29
+add $zero, $zero, $zero, 0			#pc:36, nop
+mul $r12, $r7, $r5, 0				#pc:37, address1 = i*N
+add $r12, $r12, $r9, 0				#pc:38, address1 = i*N + k = address1 in memory
+mul $r15, $r9, $r5, 0				#pc:39, address2 = k*N
+add $r15, $r15, $r8, 0				#pc:40, address2 = k*N + j
+add $r15, $r15, $r3, 0				#pc:41, address2 = address2 in memory
+lw $r2, $r12, $zero, 0				#pc:42, R2 = MEM[i*N + k] = Y[i][k]
+lw $r14, $r15, $zero, 0				#pc:43, R14 = MEM[k*N + j] = Z[k][j]
+mul $r6, $r2, $r14, 0				#pc:44, R6 = R2 * R14 = Y[i][k] * Z[k][j]
+add $r13, $r13, $r6, 0				#pc:45, r = r + Y[i][k] * Z[k][j]
+add $r9, $r9, $imm, 1				#pc:46, k++
+beq $imm, $zero, $zero, 31			#pc:47, jump to start of k loop
+add $zero, $zero, $zero, 0			#pc:48, nop
+mul $r2, $r7, $r5, 0				#pc:49, R2 = i * N	this was 41
+add $r2, $r2, $r8, 0				#pc:50, R2 = i * N + j
+add $r2, $r2, $r4, 0				#pc:51, R2 = address of X[i][j] in memory
+lw $r6, $r2, $zero, 0				#pc:52, R6 = MEM[i*N + j] = X[i][j]
+add $r6, $r6, $r13, 0				#pc:53, R6 = R6 + r
+sw $r6, $r2, $zero, 0				#pc:54, X[i][j] = R6 = X[i][j] + r
+add $r8, $r8, $imm, 1				#pc:55, j++
+beq $imm, $zero, $zero, 23			#pc:56, jump to start of j loop
+add $zero, $zero, $zero, 0			#pc:57, nop
+add $r7, $r7, $imm, 1				#pc:58, i++	this was 49
+beq $imm, $zero, $zero, 20			#pc:59, jump to start of i loop
+add $zero, $zero, $zero, 0			#pc:60, nop
+add $r11, $r11, $imm, 8				#pc:61, kk =  kk + B	this was 51
+beq $imm, $zero, $zero, 17			#pc:62, jump to start of kk loop
+add $zero, $zero, $zero, 0			#pc:63, nop
+add $r10, $r10, $imm, 8				#pc:64, jj =  jj + B	this was 53
+beq $imm, $zero, $zero, 14			#pc:65, jump to start of jj loop
+add $zero, $zero, $zero, 0			#pc:66, nop
+halt $zero, $zero, $zero, 0			#pc:67, HALT execution this was 55
